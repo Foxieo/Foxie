99fafd69e1b0a2b9727fae76b6f0f1787e7ee23e
kris <sleptcovakristina89@mail.ru>
2023-02-13 14:51:52 +0900

    Proekt windows

---
+++ app/src/main/java/com/example/androidlessonsgovorov/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidlessonsgovorov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Debug;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
